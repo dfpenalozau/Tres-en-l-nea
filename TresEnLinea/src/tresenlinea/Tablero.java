055cee477b33269877c3281febea4eeb8a516489
estudiantes <estudiantes@labing00>
2017-03-10 17:42:13 -0500

    Gráfica creada

---
+++ TresEnLinea/src/tresenlinea/Tablero.java
@@ -0,0 +1,16 @@
+package tresenlinea;
+
+
+public class Tablero {
+
+public char[][]Ficha=new char[3][3];
+
+public int Turno;
+
+public void CalcularTriqui(){
+}
+
+public void Jugar(){
+
+}
+}
---
+++ TresEnLinea/src/tresenlinea/TresEnLinea.java
@@ -0,0 +1,12 @@
+package tresenlinea;
+
+public class TresEnLinea {
+
+    public static void main(String[] args) {
+    
+        Tablero miTablero;
+        miTablero=new Tablero();
+        
+    }
+    
+}
